eafa2891b0179bed63858ad41671f59d90ec0982
Noam Franco <noamf@ad.checkpoint.com>
2021-12-16 20:00:17 +0200

    dos protector

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'dosprotecer',         # How you named your package folder (MyLib)
-  packages = ['dosprotecer'],   # Chose the same as "name"
+  name = 'dosprotecter',         # How you named your package folder (MyLib)
+  packages = ['dosprotecter'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Package that protect servers from dos and ddos attacks',   # Give a short description about your library
