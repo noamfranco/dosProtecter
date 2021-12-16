3073c1e800b3e4ca43b38983150f3eb0d1e4f17f
Noam Franco <noamf@ad.checkpoint.com>
2021-12-16 19:57:25 +0200

    dos protector

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Noam Franco',                   # Type in your name
   author_email = 'noamfranco2@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/noamfranco/dosProtecter',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/noamfranco/dosProtecter/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/noamfranco/dosProtecter/archive/tag.tar.gz',    # I explain this later on
   keywords = ['Cyber', 'Dos', 'Ddos'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'time',
