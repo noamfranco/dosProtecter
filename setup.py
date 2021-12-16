2ae0d8ceafe9b8247719a0c2220d28c470d700a4
Noam Franco <noamf@ad.checkpoint.com>
2021-12-16 19:48:05 +0200

    dos protector

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author_email = 'noamfranco2@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/noamfranco/dosProtecter',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/noamfranco/dosProtecter/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  keywords = ['Cyber', 'Dos', 'Ddos'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'time',
           'smtplib',
